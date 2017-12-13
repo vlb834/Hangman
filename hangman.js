3b0f54f2071f8f1ac40d0b3474841652d03c7ca9
vlb834 <vlb883344@gmail.com>
2017-12-13 23:31:09 +0000

    promise recursion working! need to fix final result...

--- hangman.js
+++ hangman.js
@@ -8,39 +8,40 @@ let word = '';
 let hangman = [];
 const displayHangman = () => console.log(chalk.blue(hangman.join('')));
 
-prompt('Enter a word for Hangman: ')
-    .then(function setUpGame(val) {
+function start(val) {
+    console.log(chalk.yellow('H A N G M A N. You need to guess the ' + word.length + '-letter word: '));
+    displayHangman();
+    prompt('Guess a letter: ').then(guess);
+}
+
+function guess(val) {
+    if (hangman.includes('_ ')) {
+        if (word.includes(val)) {
+            console.log(chalk.green('You guessed a letter correctly!'));
+            let index = word.indexOf(val);
+            let letter = word.charAt(index);
+            word = word.replace(letter, '_');
+            hangman.splice(index, 1, val + ' ');
+            displayHangman(); 
+            prompt('Guess another letter: ').then(guess);
+        } else {
+            console.log(chalk.red('Wrong!'));
+            prompt('Guess another letter: ').then(guess);
+        }
+    } else {
+        console.log(chalk.green('You guessed the word!'));
+        displayHangman(); 
+        prompt.done();
+    }
+}
+
+prompt('Enter a word for Hangman: ').then(function setupGame(val) {
         word = val;
         for (let i = 0; i < val.length; i++) {
             hangman.push('_ ');
         }    
         console.log('The hangman word is: ' + word);
-        return prompt('Press any key to start playing.');
-    })
-    .then(function start(val) {
-        console.log(chalk.yellow('H A N G M A N. You need to guess the ' + word.length + '-letter word: '));
-        displayHangman();
-        return prompt('Guess a letter: ');
-    })
-    .then(function guess(val) {
-        if (hangman.includes('_ ')) {
-            if (word.includes(val)) {
-                console.log(chalk.green('You guessed a letter correctly!'));
-                let index = word.indexOf(val);
-                let letter = word.charAt(index);
-                word = word.replace(letter, '_');
-                hangman.splice(index, 1, val + ' ');
-                displayHangman(); 
-                return prompt('Guess another letter: ');
-            } else {
-                console.log(chalk.red('Wrong!'));
-                return prompt.guess('Guess another letter: ');
-            }
-        } else {
-            console.log(chalk.green('You guessed the word!'));
-            displayHangman(); 
-            prompt.done();
-        }
+        prompt('Press any key to start playing.').then(start);
     })
     .catch (function rejected(err) {
         console.log('error: ', err.stack);
