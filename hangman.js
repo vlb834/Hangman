16fd546abab9b98e33c30bfd8bb1f1200a039ac6
vlb834 <vlb883344@gmail.com>
2017-12-14 11:29:30 +0000

    separated out functions from prompts

--- hangman.js
+++ hangman.js
@@ -1,13 +1,14 @@
 const prompt = require('prompt-promise');
 const chalk = require('chalk');
-process.setMaxListeners(100);
 
 console.log(chalk.yellow('H A N G M A N'));
 
+// VARIABLES 
 let word = '';
 let hangman = [];
 const displayHangman = () => console.log(chalk.blue(hangman.join('')));
 
+// FUNCTIONS 
 function start(val) {
     console.log(chalk.yellow('H A N G M A N. You need to guess the ' + word.length + '-letter word: '));
     displayHangman();
@@ -16,35 +17,42 @@ function start(val) {
 
 function guess(val) {
     if (hangman.includes('_ ')) {
-        if (word.includes(val)) {
-            console.log(chalk.green('You guessed a letter correctly!'));
-            let index = word.indexOf(val);
-            let letter = word.charAt(index);
-            word = word.replace(letter, '_');
-            hangman.splice(index, 1, val + ' ');
-            displayHangman(); 
-            prompt('Guess another letter: ').then(guess);
-        } else {
-            console.log(chalk.red('Wrong!'));
-            prompt('Guess another letter: ').then(guess);
-        }
+        checkLetter(val);
+        prompt('Guess another letter: ').then(guess);
     } else {
         console.log(chalk.green('You guessed the word!'));
-        displayHangman(); 
+        displayHangman();
         prompt.done();
     }
 }
 
+function checkLetter(val) {
+    // if (val != ) check not a number
+    if (val.length > 1) {
+        console.log(chalk.red('Incorrect input type - too many letters!'));
+    } else if (word.includes(val)) {
+        console.log(chalk.green('You guessed a letter correctly!'));
+        let index = word.indexOf(val);
+        let letter = word.charAt(index);
+        word = word.replace(letter, '_'); // hold this in an array an pop off an array. 
+        hangman.splice(index, 1, val + ' ');
+        displayHangman();
+    } else {
+        console.log(chalk.red('Wrong!'));
+    }
+}
+
+// COMMAND LINE PROMPTS
+
 prompt('Enter a word for Hangman: ').then(function setupGame(val) {
-        word = val;
-        for (let i = 0; i < val.length; i++) {
-            hangman.push('_ ');
-        }    
-        console.log('The hangman word is: ' + word);
-        prompt('Press any key to start playing.').then(start);
-    })
-    .catch (function rejected(err) {
-        console.log('error: ', err.stack);
-        prompt.finish();
-    });
+    word = val;
+    for (let i = 0; i < val.length; i++) {
+        hangman.push('_ ');
+    }
+    console.log('The hangman word is: ' + word);
+    prompt('Press any key to start playing.').then(start);
+}).catch(function rejected(err) {
+    console.log('error: ', err.stack);
+    prompt.finish();
+});
 
