f28437f02401598131d4fc78cd9e70067f87225e
vlb834 <vlb883344@gmail.com>
2017-12-16 15:47:39 +0000

    Game over fixed

--- hangman.js
+++ hangman.js
@@ -5,15 +5,16 @@ console.log(chalk.yellow('H A N G M A N'));
 
 // GAME VARIABLES 
 let word = ''; // value declared in setupGame function
-let tracker = ''; // mutable word proxy to display hangman to player correctly
+let tracker = ''; // mutable word proxy to display guesses to player correctly
 let count = 0; // word.length value declared in setupGame function
 let hangman = []; // displayed to player
-let countdown = []; // needed to determine if word guess is complete
+let complete = []; // needed to determine if word guess is complete
 let turns = 0; // guess turns left before you hang
 const validInputs = /^[a-zA-Z]+$/; // check everything from start to end of string contains only letters
-const displayHangman = () => console.log(chalk.magenta(hangman.join('')));
+const displayHangman = () => console.log(chalk.yellow(hangman.join('')));
 
 // FUNCTIONS
+
 function checkWordInputValid(wordInput) {
     return validInputs.test(wordInput);
 }
@@ -33,30 +34,32 @@ function checkLetterInputValid(letterInput) {
 
 function passLetter(letter) {
     let index = tracker.indexOf(letter);
-    countdown.pop();
+    complete.pop();
     tracker = tracker.replace(letter, 0);
     hangman.splice(index, 1, letter + ' ');
+}
+
+
+function correct(letter) {
+    console.log(chalk.green('You guessed a letter correctly!'));
+    passLetter(letter);
+}
+
+function wrong(letter) {
+    turns = turns + 1;
+    let remainingTurns = 6 - turns; // displayed to player
+    hangmanFigure.drawLevel(turns, 'magenta');
+    console.log(chalk.red('Wrong!'));
+    console.log(chalk.red('You have ' + remainingTurns + ' incorrect guesses left.'));
 }
 
 function checkLetter(letterInput) {
     if (checkLetterInputValid(letterInput)) {
         let letter = letterInput.toLowerCase();
         if (tracker.includes(letter)) {
-            console.log(chalk.green('You guessed a letter correctly!'));
-            passLetter(letter);
+            correct(letter);
         } else {
-            // move all of this into a new incorrect Guess function to pass in. 
-            turns = turns + 1;
-            let remainingTurns = 6 - turns;
-            hangmanFigure.drawLevel(turns, 'magenta');
-            console.log(chalk.red('Wrong!'));
-            if (turns == 6) {
-                console.log(chalk.red('G A M E  O V E R'));
-                console.log(chalk.red('You ran out of incorrect guesses.'));
-                prompt('Play again? Yes or No: ').then(playAgain); //this exits back to guess function. NEED TO FIX
-            } else {
-                console.log(chalk.magenta('You have ' + remainingTurns + ' incorrect guesses left.'));
-            } 
+            wrong(letter);
         }
     } else {
         return;
@@ -65,7 +68,7 @@ function checkLetter(letterInput) {
 
 function checkGuess(letterInput) {
     if (letterInput.toLowerCase() === word) {
-        countdown = [];
+        complete = [];
         hangman = word.toLowerCase().split('');
     } else {
         checkLetter(letterInput);
@@ -73,21 +76,33 @@ function checkGuess(letterInput) {
     displayHangman();
 }
 
-function guess(letterInput) {
+function guess(letterInput) { // guess recursion plus winning/game over results
     checkGuess(letterInput);
-    if (countdown.length == 0) {
-        console.log(chalk.green('You guessed the word!')); prompt('Play again? Yes or No: ').then(playAgain);
+    if (complete.length == 0) {
+        console.log(chalk.green('You guessed the word!')); 
+        prompt('Play again? Yes or No: ').then(playAgain);
+    } else if (turns == 6) {
+            console.log(chalk.red('G A M E  O V E R'));
+            console.log(chalk.red('You ran out of incorrect guesses.'));
+            prompt('Play again? Yes or No: ').then(playAgain); 
     } else {
         prompt('Guess another letter: ').then(guess);
     }
 }
 
 function start(anyKey) {
+    hangmanFigure.drawLevel(0, 'magenta');
+    hangmanFigure.drawLevel(1, 'magenta');
+    hangmanFigure.drawLevel(2, 'magenta');
+    hangmanFigure.drawLevel(3, 'magenta');
+    hangmanFigure.drawLevel(4, 'magenta');
+    hangmanFigure.drawLevel(5, 'magenta');
+    hangmanFigure.drawLevel(6, 'magenta');
     console.log(chalk.yellow('H A N G M A N.'));
-    console.log(chalk.yellow('You need to guess the ' + count + '-letter word: '));
-    displayHangman();
+    console.log(chalk.yellow('You need to guess the ' + count + '-letter word.'));
     console.log(chalk.yellow('You have 6 incorrect guesses before you hang!'));
     hangmanFigure.drawLevel(turns, 'magenta');
+    displayHangman();    
     prompt('Guess a letter: ').then(guess);
 }
 
@@ -96,7 +111,7 @@ function setupGame(wordInput) {
         word = wordInput.toLowerCase();
         tracker = wordInput.toLowerCase();
         count = wordInput.length;
-        countdown = word.split('');
+        complete = word.split('');
         for (let i = 0; i < count; i++) {
             hangman.push('_ ');
         }
@@ -117,7 +132,8 @@ function playAgain(answer) {
         tracker = '';
         count = 0;
         hangman = [];
-        countdown = [];
+        complete = [];
+        turns = 0;
         newGame();
     } else if (answer === 'no' || answer === 'n') {
         console.log((chalk.yellow('Thanks for playing! Bye Bye!')));
